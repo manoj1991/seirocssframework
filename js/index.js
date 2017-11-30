41fa40c6b2743b639117ce68f593aadf56037dae
Nadia Rodriguez <nadsr2@gmail.com>
2017-11-29 23:38:08 -0800

    test website

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("body").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("body").style.marginLeft = "250px";    
+}
