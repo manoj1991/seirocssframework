6757a8e101af8db48042689f0154a8877c4805f1
Nadia Rodriguez <nadsr2@gmail.com>
2017-12-14 22:51:52 -0800

    nav menu fix

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ function openNav() {
 }
 
 function closeNav() {
-    document.getElementById("sidenav").style.width = "250px";
-    document.getElementById("body").style.marginLeft = "250px";    
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("body").style.marginLeft = "0";   
+    document.getElementById("open").classList.remove("s-hidden");
+    document.getElementById("close").classList.add("s-hidden"); 
 }
