3b9bb8cbeb04bf812b8325d9021a3ec3dfc193f1
Nadia Rodriguez <nadsr2@gmail.com>
2017-12-14 22:42:47 -0800

    nav menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
     document.getElementById("body").style.marginLeft = "250px";
+    document.getElementById("open").classList.add("s-hidden");
+    document.getElementById("close").classList.remove("s-hidden");
 }
 
 function closeNav() {
